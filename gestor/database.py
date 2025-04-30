fc50caa9c8f8f55148c3bb5adb01294bc1ac226b
jeffernovoa <jefferng35@gmail.com>
2025-05-01 00:12:47 +0200

    ahi vamos

--- gestor/database.py
+++ gestor/database.py
@@ -1,3 +1,5 @@
+import csv
+
 class Cliente:
     def __init__(self, dni, nombre, apellido):
         self.nombre = nombre
@@ -8,9 +10,20 @@ class Cliente:
         return f"({self.dni}) {self.nombre} {self.apellido}"
     
 class Clientes:
-
-    # Lista de clientes
+    # Creamos la lista y cargamos los clientes en memoria
     lista = []
+    with open("clientes.csv", newline="\n") as fichero:
+        reader = csv.reader(fichero, delimiter=";")
+        for dni, nombre, apellido in reader:
+            cliente = Cliente(dni, nombre, apellido)
+            lista.append(cliente)
+
+    @staticmethod
+    def guardar():
+        with open("clientes.csv", "w", newline="\n") as fichero:
+            writer = csv.writer(fichero, delimiter=";")
+            for c in Clientes.lista:
+                writer.writerow((c.dni, c.nombre, c.apellido))
 
     @staticmethod
     def buscar(dni):
@@ -22,6 +35,7 @@ class Clientes:
     def crear(dni, nombre, apellido):
         cliente = Cliente(dni, nombre, apellido)
         Clientes.lista.append(cliente)
+        Clientes.guardar()
         return cliente
     
     @staticmethod
@@ -30,6 +44,7 @@ class Clientes:
             if cliente.dni == dni:
                 Clientes.lista[i].nombre = nombre
                 Clientes.lista[i].apellido = apellido
+                Clientes.guardar()
                 return Clientes.lista[i]
             
     @staticmethod
@@ -37,4 +52,6 @@ class Clientes:
         for i, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
                 cliente = Clientes.lista.pop(i)
+                Clientes.guardar()
                 return cliente
+            
--- gestor/menu.py
+++ gestor/menu.py
@@ -32,8 +32,13 @@ def iniciar():
 
         if opcion == '3':
             print("Añadiendo un cliente...\n")
-            dni = helpers.leer_texto(
-                3, 3, "DNI (2 ints y 1 char)").upper()
+
+            # Comprobamos que el DNI es válido
+            while 1:
+                dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+                if helpers.dni_valido(dni, db.Clientes.lista):
+                    break
+                
             nombre = helpers.leer_texto(
                 2, 30, "Nombre (de 2 a 30 chars)").capitalize()
             apellido = helpers.leer_texto(
